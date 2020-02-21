655c1697f9e5cf21d2f2a31949af42f70a1d21a5
Joseph Bruno <brunojoey12@gmail.com>
2020-02-21 16:49:35 -0600

    added variables for the text area, the checkboxes in the form, and the text area element formula from class to have a template.

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var textAreaEl = document.querySelector('textarea');
+var upperCaseBox = document.querySelector("#uppercaseBox");
+var lowerCaseBox = document.querySelector("#lowercaseBox");
+var numbersBox = document.querySelector("#numbersBox");
+var specialCharactersBox = document.querySelector("#specialCharactersBox");
+var 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = password
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+textAreaEl.addEventListener("keyup", function(event) {
+  var key = event.key.toLowerCase();
+  var alphabetNumericSpecialCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()./,+".split("");
+  var upperCase = event.key.toUpperCase();
+  if (alphabetNumericCharacters.includes(key)) {
+    elements.forEach(function(element) {
+      element.textContent += event.key;
+      element.textContent += event.upperCase;
+    });
+  } 
+});
