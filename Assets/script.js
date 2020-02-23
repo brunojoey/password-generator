aecfe58b5294bfc8a0f2a27dd374753158944070
Joseph Bruno <brunojoey12@gmail.com>
2020-02-23 15:12:08 -0600

    Changed the checkbox forms in first few lines to be shown as false if not checked.

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,10 @@
-var passwordLengthEl = document.getElementById('passwordLength');
-var uppercaseEl = document.getElementById('uppercase');
-var lowercaseEl = document.getElementById('lowercase');
-var numbersEl = document.getElementById('numbers');
-var symbolsEl = document.getElementById('symbols');
+var passwordLengthEl = document.getElementById('passwordLength').value;
+var uppercaseEl = document.getElementById('uppercase').checked;
+var lowercaseEl = document.getElementById('lowercase').checked;
+var numbersEl = document.getElementById('numbers').checked;
+var symbolsEl = document.getElementById('symbols').checked;
 var generateEl = document.getElementById('generate');
+console.log(uppercaseEl);
 
 function writePassword() {
   var password = generatePassword();
@@ -13,52 +14,69 @@ function writePassword() {
   for (var i = 0; i < passwordLength.length; i++){
     return (password);
   }
-  console.log(passwordLengthEl);
-  writePassword();
+  function getUppercase() {
+    var passwordText = document.querySelector("#password");
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    if (uppercaseEl.checked === true) {
+      passwordText.style.display = "block";
+      return uppercase;
+    } else {
+      passwordText.style.display = "none";
+    } 
+  } passwordText = getUppercase();
+  
 }
 
 function getUppercase() {
   var passwordText = document.querySelector("#password");
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   if (uppercaseEl.checked === true) {
     passwordText.style.display = "block";
+    return uppercase;
   } else {
-    passwordText.style.display = "block";
+    passwordText.style.display = "none";
   } 
-} getUppercase();
+} passwordText = getUppercase();
 
 function getLowerCase() {
   var passwordText = document.querySelector("#password");
+  var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
 if (lowercaseEl.checked === true) {
   passwordText.style.display = "block";
+  return lowercase;
 } else {
-  passwordText.style.display = "block";
+  passwordText.style.display = "none";
 } 
-} getLowerCase();
+} passwordText = getLowerCase();
 
 function getNumbers() {
   var passwordText = document.querySelector("#password");
+  var numbers = "0123456789".split("");
   if (numbersEl.checked === true) {
     passwordText.style.display = "block";
+    return numbers;
   } else {
-    passwordText.style.display = "block";
+    passwordText.style.display = "none";
   } 
-} getNumbers();
+} passwordText = getNumbers();
 
 function getSymbols() {
   var passwordText = document.querySelector("#password");
+  var symbols= "!@#$%^&**()./,+=".split("");
   if (symbolsEl.checked === true) {
     passwordText.style.display = "block";
+    return symbols;
   } else {
-    passwordText.style.display = "block";
+    passwordText.style.display = "none";
   } 
-} getSymbols();
+} passwordTExt = getSymbols();
 
 function getRandomLetters(max,min) {
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
     var numbers = "0123456789".split("");
     var symbols= "!@#$%^&**()./,+=".split("");
-    var lengthPw = Math.floor(Math.random(passwordLengthEl) * (max - min + 1)) + min;
+    var lengthPw = Math.floor(Math.random() * (max - min + 1)) + min;
     var passwordRandom = Array(lengthPw).getElementById("uppercase", "lowercase", "numbers", "symbols", "passwordLength");
     return passwordRandom;
 }
@@ -67,4 +85,3 @@ generateEl.addEventListener("click", function() {
   var passwordText = document.querySelector("#password");
   document.getElementById("password").value = passwordText;
 });
-
