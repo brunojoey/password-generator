1ca321e39d7e3070d4a824b1c795c9d84471d38b
Joseph Bruno <brunojoey12@gmail.com>
2020-02-23 14:38:00 -0600

    Changed functions around a bit.

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
-var getPassword = document.querySelector("#password");
 var passwordLengthEl = document.getElementById('passwordLength');
 var uppercaseEl = document.getElementById('uppercase');
 var lowercaseEl = document.getElementById('lowercase');
@@ -6,70 +5,66 @@ var numbersEl = document.getElementById('numbers');
 var symbolsEl = document.getElementById('symbols');
 var generateEl = document.getElementById('generate');
 
-
-// function getRandomLetters(max,min) {
-//     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-//     var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
-//     var numbers = "0123456789".split("");
-//     var symbols= "!@#$%^&**()./,+=".split("");
-//     var lengthPw = Math.floor(Math.random(passwordLengthEl) * (max - min + 1)) + min;
-//     var passwordRandom = Array(lengthPw).textContent(uppercase, lowercase, numbers, symbols);
-//     return passwordRandom;
-// }
-
-// generateEl.addEventListener("click", function(){
-//     document.getElementById("password").textContent = getPassword;
-// });
-
-
-// generateEl.addEventListener("click", function() {
-//   document.getElementById("password").value = getPassword;
-// });
-
-function getUppercase() {
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  if (uppercaseEl.checked === true) {
-    getPassword.style.display = "block";
-} else {
-  uppercaseEl.style.display = "none";
-}
-}
-
-function getLowerCase() {
-  if (lowercaseEl.checked === true) {
-    var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
-    getPassword.style.display = "block";
-} else {
-  lowercaseEl.style.display = "none";
-}
-}
-
-function getNumbers() {
-  if (numbersEl.checked === true) {
-    var numbers = "0123456789".split("");
-    getPassword.style.display = "block";
-} else {
-  numbersEl.style.display = "none";
-}
-}
-
-function getSymbols() {
-  var symbols = "!@#$%^&**()./,+=".split("");
-  if (symbolsEl.checked === true) {
-    getPassword.style.display = "block";
-} else {
-  symbolsEl.style.display = "none";
-}
-}
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password
-    for (var i = 0; i < passwordLength.length; i++){
-      return (password);
-    }
-    console.log(passwordLengthEl);
-    writePassword();
+  for (var i = 0; i < passwordLength.length; i++){
+    return (password);
+  }
+  console.log(passwordLengthEl);
+  writePassword();
 }
+
+function getUppercase() {
+  var passwordText = document.querySelector("#password");
+  if (uppercaseEl.checked === true) {
+    passwordText.style.display = "block";
+  } else {
+    passwordText.style.display = "block";
+  } 
+} getUppercase();
+
+function getLowerCase() {
+  var passwordText = document.querySelector("#password");
+if (lowercaseEl.checked === true) {
+  passwordText.style.display = "block";
+} else {
+  passwordText.style.display = "block";
+} 
+} getLowerCase();
+
+function getNumbers() {
+  var passwordText = document.querySelector("#password");
+  if (numbersEl.checked === true) {
+    passwordText.style.display = "block";
+  } else {
+    passwordText.style.display = "block";
+  } 
+} getNumbers();
+
+function getSymbols() {
+  var passwordText = document.querySelector("#password");
+  if (symbolsEl.checked === true) {
+    passwordText.style.display = "block";
+  } else {
+    passwordText.style.display = "block";
+  } 
+} getSymbols();
+
+function getRandomLetters(max,min) {
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+    var numbers = "0123456789".split("");
+    var symbols= "!@#$%^&**()./,+=".split("");
+    var lengthPw = Math.floor(Math.random(passwordLengthEl) * (max - min + 1)) + min;
+    var passwordRandom = Array(lengthPw).getElementById("uppercase", "lowercase", "numbers", "symbols", "passwordLength");
+    return passwordRandom;
+}
+
+generateEl.addEventListener("click", function() {
+  var passwordText = document.querySelector("#password");
+  document.getElementById("password").value = passwordText;
+});
+
